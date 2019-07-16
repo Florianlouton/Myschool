3bfa0f7186ba225f50120b9023e8fb711b86def3
unknown <florian.lou63@gmail.com>
2019-07-16 21:41:09 +0200

    initial commit

---
+++ src/test/java/dev/java/Myschool/MyschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.Myschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
